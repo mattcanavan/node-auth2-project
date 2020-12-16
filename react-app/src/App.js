5f181e78441ad8caa8d7b78a2b6c21710d7d2b81
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-12-15 19:53:43 -0800

    Login component completed. Need to test.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sign-up</h1>
+
+      {/* Header */}
+
+      {/* Register */}
+
+      {/* Login */}
+      <Login/>
+
+      {/* Home (authorized) */}
+
     </div>
   );
 }
